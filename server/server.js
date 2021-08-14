dbfa9aca67b0e89ba35e9b916ab97c554f1044f3
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2021-08-14 21:55:52 +1000

    React Server Side Rendered

---
+++ server/server.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+import HTTP from "http";
+import express from "express";
+import fs from "fs";
+import { Server } from "socket.io";
+import { voiceMp3 } from "../functions/VoiceOver.js";
+import React from "react";
+import ReactDOMServer from "react-dom/server.js";
+const app = express();
+const http = HTTP.createServer(app);
+const io = new Server(http);
+import App from "../src/App.js";
+
+app.use(require("cors")({ origin: "*" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("./build"));
+
+app.get("/", (req, res) => {
+  fs.readFile("./build/index.html", "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Some error happened");
+    }
+    return res.send(
+      data.replace(
+        '<div id="root"></div>',
+        `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
+      )
+    );
+  });
+});
+
+app.post("/voiceover", async (req, res) => {
+  const { text, outputFile } = req.body;
+  const data = await voiceMp3(text, outputFile);
+  res.json(data);
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected");
+  socket.on("Working", (cb) => cb("Working."));
+});
+
+http.listen(process.env.PORT || 3000, () => console.log(process.env.URL));
